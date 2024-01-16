b33b2ed5078445489ac9a996daea21f9ecc08d46
Shanliang Yao <1329517386@qq.com>
2024-01-16 17:13:59 +0800

    Update track_waterscenestracking_dla34.sh

--- experiments/track_waterscenestracking_dla34.sh
+++ experiments/track_waterscenestracking_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python track.py mot --load_model ../exp/mot/mot20_dla34/model_last.pth --conf_thres 0.6
+python track.py mot --load_model ../exp/mot/waterscenestracking_dla34/model_last.pth --conf_thres 0.6
 cd ..
