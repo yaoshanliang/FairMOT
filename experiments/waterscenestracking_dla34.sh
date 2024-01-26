cdda25a7f25c93d55809f5723942eac6a6915a8f
Shanliang Yao <1329517386@qq.com>
2024-01-26 20:59:54 +0800

    update

--- experiments/waterscenestracking_dla34.sh
+++ experiments/waterscenestracking_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id waterscenestracking_dla34 --data_cfg '../src/lib/cfg/waterscenestracking.json' --gpus 0 --batch_size 4
+python train.py mot --exp_id USVTrack_dla34 --data_cfg '../src/lib/cfg/USVTrack.json' --gpus 1,2,3 --batch_size 64
 cd ..
